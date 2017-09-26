6713a29d565a6b8b2d7136408dffe40071fe7167
aaronsato <2asato@gmail.com>
2017-09-25 18:11:12 -0700

    added nav bar

--- public/app.js
+++ public/app.js
@@ -3,12 +3,27 @@ console.log("play ball");
 const app = angular.module('p5', []);
 
 app.controller('mainController', ['$http', function($http) {
-    this.message = 'controller works'
+    let controller = this;
 
+this.message = "controller works";
+this.url = 'http://localhost:3000'
 
+// teams
 $http({
   method: 'GET',
-  url: 'http://localhost:3000/teams',
-}).then(response => console.log(response))
-  .catch(err => console.log(err));
+  url: this.url + '/teams',
+}).then(function(response) {
+  console.log(response)
+  this.teams = response.data;
+}.bind(this))
+
+// players
+$http({
+  method: 'GET',
+  url: this.url + '/players',
+}).then(function(response) {
+  console.log(response)
+  this.players = response.data;
+}.bind(this))
+
 }]);
