a7dbf608937eff4614e42e6326c0082e0be24e13
aaronsato <2asato@gmail.com>
2017-09-25 18:18:36 -0700

    added main section

--- public/app.js
+++ public/app.js
@@ -7,6 +7,21 @@ app.controller('mainController', ['$http', function($http) {
 
 this.message = "controller works";
 this.url = 'http://localhost:3000'
+this.team = {};
+this.teams = [];
+this.player = {};
+this.players = [];
+this.teamPlayers = [];
+this.formdata = {};
+this.showMain = true;
+this.showTeams = false;
+this.showTeamPage = false;
+this.showPlayers = false;
+this.showPlayerPage = false;
+this.showLogin = false;
+this.showRegister = false;
+this.showEdit = false;
+
 
 // teams
 $http({
