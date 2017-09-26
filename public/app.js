7674f9cc07897b1f5a766c3ee2a1d54d642f4196
aaronsato <2asato@gmail.com>
2017-09-25 17:20:00 -0700

    logged data from backend

--- public/app.js
+++ public/app.js
@@ -4,4 +4,11 @@ const app = angular.module('p5', []);
 
 app.controller('mainController', ['$http', function($http) {
     this.message = 'controller works'
+
+
+$http({
+  method: 'GET',
+  url: 'http://localhost:3000/teams',
+}).then(response => console.log(response))
+  .catch(err => console.log(err));
 }]);
